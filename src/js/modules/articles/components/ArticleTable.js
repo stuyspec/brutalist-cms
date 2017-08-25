113aeadb8b5c0dbc2fad3693574dbe45057db466
Jason Kao <jasonkao85@gmail.com>
2017-08-25 18:15:33 -0400

    completed article table form

--- src/js/modules/articles/components/ArticleTable.js
+++ src/js/modules/articles/components/ArticleTable.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { connect } from "react-redux";
 
-import { TableForm } from "../../core/components";
+import ArticleTableForm from "./ArticleTableForm";
 
-const ArticleTable = ({ articles }) => {
+const ArticleTable = () => {
   const handleSubmit = values => {
     console.log(values);
   };
   return (
     <div>
-      <TableForm data={ articles } module="articles" onSubmit={ handleSubmit }/>
+      <ArticleTableForm onSubmit={ handleSubmit }/>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  articles: state.articles.articles,
-});
-
-export default connect(mapStateToProps)(ArticleTable);
+export default ArticleTable;
